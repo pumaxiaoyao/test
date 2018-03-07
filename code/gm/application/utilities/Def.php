c5b8127489f25f705696f224063a0f8d3fc97a9a
alien <alien@8c6f7132-b775-4384-bfcf-8eb8f0ca24e1>
2018-03-07 13:33:19 +0000

    更新

---
+++ code/gm/application/utilities/Def.php
@@ -0,0 +1,107 @@
+<?php
+
+class PlatformType
+{
+    const MAIN = 1;
+    const IBC = 2;
+}
+
+class CheckStatus
+{
+    const wait = 1;
+    const agree = 2;
+    const refuse = 4;
+}
+
+class BankCardStatus
+{
+    const available = 1;
+    const unavailable = 2;
+	const delete = 3;
+}
+
+class MessageStatus
+{
+    const new = 1;
+    const read = 2;
+    const delete = 3;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//玩家
+class PlayerStatus
+{
+    //待审核
+    const check = 1;
+    //正常
+    const ok = 2;
+    //锁定
+    const lock = 3;
+    //审核失败
+    const checkFail = 4;
+}
+
+class PlayerRecordLoginType
+{
+    const login = 1;
+	const logout = 2;
+}
+
+class PlayerRecordBalanceType
+{
+    //存款
+    const deposit = 1;
+	//取款
+	const withdrawal = 2;
+	//转入
+	const transactIn = 4;
+	//转出
+	const transactOut = 8;
+	//返水
+	const rebate = 16;
+	//红利
+	const bonus = 32;
+	//存款优惠
+	const depositBonus = 64;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//代理
+
+class AgentStatus
+{
+    //待审核
+    const check = 1;
+	//正常
+	const ok = 2;
+	//锁定
+	const lock = 3;
+	//审核失败
+	const checkFail = 4;
+}
+
+class JointType
+{
+    const a = 1;
+    const b = 2;
+}
+
+class RateType
+{
+    const no = 0;
+	const fix = 1;
+	const flow = 2;
+}
+
+class SettleStatementStatus
+{
+    const wait = 1;
+	const check = 2;
+	const agree = 4;
+}
+
+class AgentRecordBalanceType
+{
+    const withdrawal = 1;
+	const commision = 2;
+}
